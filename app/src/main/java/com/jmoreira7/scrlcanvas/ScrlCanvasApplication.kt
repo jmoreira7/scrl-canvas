1887b3ec5862651ffb64619e828d8b8893121c19
jmoreira7 <jonas.andrade7@gmail.com>
2025-05-24 10:53:02 +0200

    Setup koin di

---
+++ app/src/main/java/com/jmoreira7/scrlcanvas/ScrlCanvasApplication.kt
@@ -0,0 +1,21 @@
+package com.jmoreira7.scrlcanvas
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class ScrlCanvasApplication : Application() {
+    val appModule = module {}
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@ScrlCanvasApplication)
+            modules(appModule)
+        }
+    }
+}
