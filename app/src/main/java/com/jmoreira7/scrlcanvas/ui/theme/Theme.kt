240a18a950385b037df4a9aa72d329385840a92f
jmoreira7 <jonas.andrade7@gmail.com>
2025-05-24 12:09:41 +0200

    Update background color

--- app/src/main/java/com/jmoreira7/scrlcanvas/ui/theme/Theme.kt
+++ app/src/main/java/com/jmoreira7/scrlcanvas/ui/theme/Theme.kt
@@ -3,14 +3,13 @@ package com.jmoreira7.scrlcanvas.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SCRLCanvasTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = lightColorScheme(
-        background = Color.DarkGray
+        background = Woodsmoke
     )
 
     MaterialTheme(
